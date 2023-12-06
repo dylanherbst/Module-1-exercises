b8fd754a2d0d107ead682f34da5fb54e5fea9077
dylanherbst <dylan.herbstwork@gmail.com>
2023-12-06 14:42:46 +0800

    updat ed questions and answers

--- exercise3.js
+++ exercise3.js
@@ -17,15 +17,18 @@ function changeMe2(){
 //Write a JavaScript program to print the current window contents.
 
 function calculateTriangleArea(side1, side2, side3) {
+    let s = (side1 + side2 + side3) / 2;
     let area = Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
 
     return area;
+
 }
 let side1 = 5;
 let side2 = 6;
 let side3 = 7;
 let triangleArea = calculateTriangleArea(side1, side2, side3);
 console.log(triangleArea);
+
 
 //Write a JavaScript program to get the website URL (loading page).
 let currentURL = window.location.href;
---
+++ exercise4.js
@@ -0,0 +1,32 @@
+// + mathmatical operator function
+function add (sum1, sum2){
+    console.log(sum1 + sum2);
+};
+console.log(add(2, 3));
+
+
+// - mathmatical operator function
+function subtract (sum1, sum2){
+    console.log(sum1 - sum2);
+};
+console.log(subtract(3, 2));
+
+
+// / mathmatical operator function
+function divide (sum1, sum2){
+    console.log(sum1 / sum2);
+};
+console.log(divide (4, 8));
+
+
+//* mathmatical operator function
+function multiply (sum1, sum2){
+    console.log(sum1 * sum2);
+};
+console.log(multiply (4, 8));
+
+//Create a function that takes the name of a person as an argument, and prints out “Hello <name>” to the console. 
+function getName (name) {
+    console.log(`Hello! ${name} its nice to meet you!`)
+};
+getName("dylan")
